ca7c86f1d392fcc5ebf732e0db69d507007592b6
Cindy Hauser <cindy.hauser24@gmx.de>
2026-09-14 11:47:33 +0200

    added the background for the game

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,6 +1,11 @@
 class BackgroundObject extends MovableObject {
 
-    constructor(imagePath) {
-         super().loadImage(imagePath);
+    width = 720;
+    height = 480;
+
+    constructor(imagePath, x) {
+        super().loadImage(imagePath);
+        this.x = x;
+        this.y = 480 - this.height;
     }
 }
